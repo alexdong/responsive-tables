ec28d4224c4357a43c4beb36427154e000d8df39
Alex Dong <alex.dong@gmail.com>
2015-06-15 16:50:48 +1200

    Add option to turn on splitTable regardless of window.width.
    If we see `<table class="responsive always-on">`, we will split the
    table regardless of the width of the window. This is useful when a table
    is not in a full width container.

--- responsive-tables.js
+++ responsive-tables.js
@@ -1,19 +1,27 @@
 $(document).ready(function() {
   var switched = false;
   var updateTables = function() {
-    if (($(window).width() < 767) && !switched ){
-      switched = true;
-      $("table.responsive").each(function(i, element) {
-        splitTable($(element));
-      });
-      return true;
+    // If the code supports `always-on`, do not rely on window width.
+    if ($('table.responsive.always-on').length > 0) {
+      $("table.responsive.always-on").each(function(i, element) {
+          splitTable($(element));
+        });
+    } else {
+      if (($(window).width() < 767) && !switched ){
+        switched = true;
+        $("table.responsive").each(function(i, element) {
+          splitTable($(element));
+        });
+        return true;
+      }
+      else if (switched && ($(window).width() > 767)) {
+        switched = false;
+        $("table.responsive").each(function(i, element) {
+          unsplitTable($(element));
+        });
+      }
     }
-    else if (switched && ($(window).width() > 767)) {
-      switched = false;
-      $("table.responsive").each(function(i, element) {
-        unsplitTable($(element));
-      });
-    }
+
   };
 
   $(window).load(updateTables);
@@ -21,32 +29,32 @@ $(document).ready(function() {
   $(window).on("resize", updateTables);
 
 
-	function splitTable(original)
-	{
+  function splitTable(original)
+  {
     var cols = original.data('columns'),
         selector = ':first-child';
     if (typeof cols !== 'undefined') selector = ':lt(' + cols + ')';
 
-		original.wrap("<div class='table-wrapper' />");
-		var copy = original.clone();
-		copy.removeClass("responsive");
+    original.wrap("<div class='table-wrapper' />");
+    var copy = original.clone();
+    copy.removeClass("responsive");
 
-		original.closest(".table-wrapper").append(copy);
-		copy.wrap("<div class='pinned' />");
-		original.wrap("<div class='scrollable' />");
+    original.closest(".table-wrapper").append(copy);
+    copy.wrap("<div class='pinned' />");
+    original.wrap("<div class='scrollable' />");
 
-		copy.find('tr').find("td:not(" + selector + "), th:not(" + selector + ")").addClass('hide');
+    copy.find('tr').find("td:not(" + selector + "), th:not(" + selector + ")").addClass('hide');
     original.find('tr').find('td' + selector + ', th' + selector).addClass('hide');
 
     setCellHeights(original, copy);
-	}
+  }
 
-	function unsplitTable(original) {
+  function unsplitTable(original) {
     original.closest(".table-wrapper").find(".hide").removeClass('hide');
     original.closest(".table-wrapper").find(".pinned").remove();
     original.unwrap();
     original.unwrap();
-	}
+  }
 
   function setCellHeights(original, copy) {
     var tr = original.find('tr'),
